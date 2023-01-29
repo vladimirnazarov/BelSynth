dd9c348da4f2bb2839d91de2d3fff5e4dbd80c41
vladimirnazarov <tooldiamond2014@gmail.com>
2023-01-29 18:01:00 +0300

    Password recovery 1 init

---
+++ app/src/main/java/com/ssrlab/assistant/ui/login/fragments/login/ResetPassword1Fragment.kt
@@ -0,0 +1,35 @@
+package com.ssrlab.assistant.ui.login.fragments.login
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.ssrlab.assistant.databinding.FragmentPassword1Binding
+import com.ssrlab.assistant.ui.login.fragments.base.BaseLaunchFragment
+
+class ResetPassword1Fragment: BaseLaunchFragment() {
+
+    private lateinit var binding: FragmentPassword1Binding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        binding = FragmentPassword1Binding.inflate(layoutInflater)
+
+        return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        val navController = findNavController()
+
+        binding.password1Back.setOnClickListener {
+            navController.popBackStack()
+        }
+    }
+}
