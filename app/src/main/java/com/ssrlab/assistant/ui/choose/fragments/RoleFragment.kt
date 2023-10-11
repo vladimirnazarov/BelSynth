df812335d7c1e53063d5d350b2aea93e584e85c5
Vladimir Nazarov <tooldiamond2014@gmail.com>
2023-10-11 16:56:22 +0300

    Beta v2.0 release revision 3

--- app/src/main/java/com/ssrlab/assistant/ui/choose/fragments/RoleFragment.kt
+++ app/src/main/java/com/ssrlab/assistant/ui/choose/fragments/RoleFragment.kt
@@ -41,25 +41,25 @@ class RoleFragment: BaseFragment() {
 
     private fun setUpButtons() {
         binding.apply {
-            roleButton1Ripple.setOnClickListener(createClickListener("assistant", 1))
-            roleButton2Ripple.setOnClickListener(createClickListener("architect", 2))
-            roleButton3Ripple.setOnClickListener(createClickListener("business_analyst", 3))
-            roleButton4Ripple.setOnClickListener(createClickListener("financial_consultant", 4))
-            roleButton5Ripple.setOnClickListener(createClickListener("recruiter", 5))
-            roleButton6Ripple.setOnClickListener(createClickListener("project_manager", 6))
-            roleButton7Ripple.setOnClickListener(createClickListener("legal_consultant", 7))
-            roleButton8Ripple.setOnClickListener(createClickListener("marketing specialist", 8))
-            roleButton9Ripple.setOnClickListener(createClickListener("realtor", 9))
-            roleButton10Ripple.setOnClickListener(createClickListener("engineer", 10))
-            roleButton11Ripple.setOnClickListener(createClickListener("programmer", 11))
-            roleButton12Ripple.setOnClickListener(createClickListener("teacher", 12))
-            roleButton13Ripple.setOnClickListener(createClickListener("writer", 13))
+            roleButton1Ripple.setOnClickListener(createClickListener("assistant", 1, binding.roleButton1Title.text.toString()))
+            roleButton2Ripple.setOnClickListener(createClickListener("architect", 2, binding.roleButton2Title.text.toString()))
+            roleButton3Ripple.setOnClickListener(createClickListener("business_analyst", 3, binding.roleButton3Title.text.toString()))
+            roleButton4Ripple.setOnClickListener(createClickListener("financial_consultant", 4, binding.roleButton4Title.text.toString()))
+            roleButton5Ripple.setOnClickListener(createClickListener("recruiter", 5, binding.roleButton5Title.text.toString()))
+            roleButton6Ripple.setOnClickListener(createClickListener("project_manager", 6, binding.roleButton6Title.text.toString()))
+            roleButton7Ripple.setOnClickListener(createClickListener("legal_consultant", 7, binding.roleButton7Title.text.toString()))
+            roleButton8Ripple.setOnClickListener(createClickListener("marketing specialist", 8, binding.roleButton8Title.text.toString()))
+            roleButton9Ripple.setOnClickListener(createClickListener("realtor", 9, binding.roleButton9Title.text.toString()))
+            roleButton10Ripple.setOnClickListener(createClickListener("engineer", 10, binding.roleButton10Title.text.toString()))
+            roleButton11Ripple.setOnClickListener(createClickListener("programmer", 11, binding.roleButton11Title.text.toString()))
+            roleButton12Ripple.setOnClickListener(createClickListener("teacher", 12, binding.roleButton12Title.text.toString()))
+            roleButton13Ripple.setOnClickListener(createClickListener("writer", 13, binding.roleButton13Title.text.toString()))
         }
     }
 
-    private fun createClickListener(role: String, roleInt: Int) : OnClickListener {
+    private fun createClickListener(role_code: String, roleInt: Int, role: String) : OnClickListener {
         return OnClickListener {
-            chooseActivity.intentToChat(chatId, chatTitle, chatImg, role, roleInt)
+            chooseActivity.intentToChat(chatId, chatTitle, chatImg, role_code, roleInt, role)
         }
     }
 }
